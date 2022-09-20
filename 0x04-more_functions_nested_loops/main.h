c192e1b09190da97ba01671b20a8b0579c475426
muntuza <mmabathomothupi@gmail.com>
2022-09-20 12:01:41 -0700

    98 Battery st.

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File - main.h
+ * Author: Genius Villagers
+ * Desc: Header contains protype for all functions
+ *	used in 0x03-more_functions_nested_loos directory
+ *
+ */
+ 
+int _putchar(char c);
+
+
+
+#endif
